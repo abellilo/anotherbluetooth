5469a16038fe56f61c0c6e26e4fe9b2a997483f5
abellilo <abel.ayinde@gmail.com>
2021-10-03 10:42:00 +0100

    anotherbluetooth

---
+++ lib/main.dart
@@ -0,0 +1,298 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BluetoothApp(),
+    );
+  }
+}
+
+class BluetoothApp extends StatefulWidget {
+  @override
+  _BluetoothAppState createState() => _BluetoothAppState();
+}
+
+class _BluetoothAppState extends State<BluetoothApp> {
+// Initializing the Bluetooth connection state to be unknown
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+
+  // Get the instance of the Bluetooth
+  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+
+  // Track the Bluetooth connection with the remote device
+  BluetoothConnection connection;
+
+  // To track whether the device is still connected to Bluetooth
+  bool get isConnected => connection != null && connection.isConnected;
+
+  // This member variable will be used for tracking
+// the Bluetooth device connection state
+  int _deviceState;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get current state
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    _deviceState = 0; // neutral
+
+    // If the Bluetooth of the device is not enabled,
+    // then request permission to turn on Bluetooth
+    // as the app starts up
+    enableBluetooth();
+
+    // Listen for further state changes
+    FlutterBluetoothSerial.instance
+        .onStateChanged()
+        .listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+
+        // For retrieving the paired devices list
+        getPairedDevices();
+      });
+    });
+  }
+
+  Future<void> enableBluetooth() async {
+    // Retrieving the current Bluetooth state
+    _bluetoothState = await FlutterBluetoothSerial.instance.state;
+
+    // If the Bluetooth is off, then turn it on first
+    // and then retrieve the devices that are paired.
+    if (_bluetoothState == BluetoothState.STATE_OFF) {
+      await FlutterBluetoothSerial.instance.requestEnable();
+      await getPairedDevices();
+      return true;
+    } else {
+      await getPairedDevices();
+    }
+    return false;
+  }
+
+  // Define a new class member variable
+// for storing the devices list
+  List<BluetoothDevice> _devicesList = [];
+
+  Future<void> getPairedDevices() async {
+    List<BluetoothDevice> devices = [];
+
+    // To get the list of paired devices
+    try {
+      devices = await _bluetooth.getBondedDevices();
+    } on PlatformException {
+      print("Error");
+    }
+
+    // It is an error to call [setState] unless [mounted] is true.
+    if (!mounted) {
+      return;
+    }
+
+    // Store the [devices] list in the [_devicesList] for accessing
+    // the list outside this class
+    setState(() {
+      _devicesList = devices;
+    });
+  }
+
+  // Define a member variable to track
+// when the disconnection is in progress
+  bool isDisconnecting = false;
+
+  @override
+  void dispose() {
+    if (isConnected) {
+      isDisconnecting = true;
+      connection.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+  bool _connected = false;
+  BluetoothDevice _device;
+
+  void _disconnect() async {
+    // Closing the Bluetooth connection
+    await connection.close();
+    print('Device disconnected');
+
+    // Update the [_connected] variable
+    if (!connection.isConnected) {
+      setState(() {
+        _connected = false;
+      });
+    }
+  }
+
+
+  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
+    List<DropdownMenuItem<BluetoothDevice>> items = [];
+    if (_devicesList.isEmpty) {
+      items.add(DropdownMenuItem(
+        child: Text('NONE'),
+      ));
+    } else {
+      _devicesList.forEach((device) {
+        items.add(DropdownMenuItem(
+          child: Text(device.name),
+          value: device,
+        ));
+      });
+    }
+    return items;
+  }
+
+  void _connect() async {
+    if (_device == null) {
+      print('No device selected');
+    } else {
+      // Making sure the device is not connected
+      if (!isConnected) {
+        // Trying to connect to the device using
+        // its address
+        await BluetoothConnection.toAddress(_device.address)
+            .then((_connection) {
+          print('Connected to the device');
+          connection = _connection;
+
+          // Updating the device connectivity
+          // status to [true]
+          setState(() {
+            _connected = true;
+          });
+
+          // This is for tracking when the disconnecting process
+          // is in progress which uses the [isDisconnecting] variable
+          // defined before.
+          // Whenever we make a disconnection call, this [onDone]
+          // method is fired.
+          connection.input.listen(null).onDone(() {
+            if (isDisconnecting) {
+              print('Disconnecting locally!');
+            } else {
+              print('Disconnected remotely!');
+            }
+            if (this.mounted) {
+              setState(() {});
+            }
+          });
+        }).catchError((error) {
+          print('Cannot connect, exception occurred');
+          print(error);
+        });
+        print('Device connected');
+      }
+    }
+  }
+
+  // Method to send message
+// for turning the Bluetooth device on
+  void _sendOnMessageToBluetooth() async {
+    connection.output.add(utf8.encode("1" + "\r\n"));
+    await connection.output.allSent;
+    print('Device Turned On');
+    setState(() {
+      _deviceState = 1; // device on
+    });
+  }
+
+// Method to send message
+// for turning the Bluetooth device off
+  void _sendOffMessageToBluetooth() async {
+    connection.output.add(utf8.encode("0" + "\r\n"));
+    await connection.output.allSent;
+    print('Device Turned Off');
+    setState(() {
+      _deviceState = -1; // device off
+    });
+  }
+  bool _isButtonUnavailable = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bluetooth Serial Plugin'),
+        ),
+        body: ListView(children: <Widget>[
+          Switch(
+            value: _bluetoothState.isEnabled,
+            onChanged: (bool value) {
+              future() async {
+                if (value) {
+                  // Enable Bluetooth
+                  await FlutterBluetoothSerial.instance.requestEnable();
+                } else {
+                  // Disable Bluetooth
+                  await FlutterBluetoothSerial.instance.requestDisable();
+                }
+
+                // In order to update the devices list
+                await getPairedDevices();
+                _isButtonUnavailable = false;
+
+                // Disconnect from any device before
+                // turning off Bluetooth
+                if (_connected) {
+                  _disconnect();
+                }
+              }
+
+              future().then((_) {
+                setState(() {});
+              });
+            },
+          ),
+          DropdownButton(
+            items: _getDeviceItems(),
+            onChanged: (value) =>
+                setState(() => _device = value),
+            value: _devicesList.isNotEmpty ? _device : null,
+          ),
+          RaisedButton(
+            onPressed: _isButtonUnavailable
+                ? null
+                : _connected ? _disconnect : _connect,
+            child:
+            Text(_connected ? 'Disconnect' : 'Connect'),
+          ),
+          // ON button
+          FlatButton(
+            onPressed: _connected
+                ? _sendOnMessageToBluetooth
+                : null,
+            child: Text("ON"),
+          ),
+// OFF button
+          FlatButton(
+            onPressed: _connected
+                ? _sendOffMessageToBluetooth
+                : null,
+            child: Text("OFF"),
+          )
+        ]),
+      ),
+    );
+  }
+}
